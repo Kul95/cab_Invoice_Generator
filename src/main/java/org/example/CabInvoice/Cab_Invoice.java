b6f2cf14e0109fff292715599ec345fd57c11a6c
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-10-03 15:45:41 +0530

    Added :UC1

---
+++ src/main/java/org/example/CabInvoice/Cab_Invoice.java
@@ -0,0 +1,15 @@
+package org.example.CabInvoice;
+
+public class Cab_Invoice {
+
+    public static final double costperkm=10.0;
+    public static final double costpermin=1.0;
+    public static final double min_fare=5;
+    public double calculateFare(double distance,int time){
+        double totalFare=(distance*costperkm)+(time*costpermin);
+        if(totalFare<5){
+            return  min_fare;
+        }
+        return  totalFare;
+    }
+}
